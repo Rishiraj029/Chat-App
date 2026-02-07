13d0be7cbb99b8353902f03bf7a7804e335df799
Rishiraj Singh <rishirajsinghrathore029@gmail.com>
2026-02-07 14:30:21 +0530

    Refactor database connection logic and improve error handling

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import "dotenv/config";
 import { connectDB } from "./src/config/database";
 import app from "./src/app";
+import { log } from "node:console";
 
 
 const PORT = process.env.PORT || 3000
@@ -14,4 +15,8 @@ connectDB().then(() => {
     console.log("Server is running on PORT: ",PORT);
     
    });
-});
+}).catch ((error) => {
+   console.log("Failed to start server: ", error);
+   process.exit(1);
+   
+})
