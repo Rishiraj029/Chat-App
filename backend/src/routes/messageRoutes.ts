2f665837eb9fb9b47ca2a7c55b25a9019eeb2aeb
Rishiraj Singh <rishirajsinghrathore029@gmail.com>
2026-02-09 16:08:48 +0530

    Implement chat, message, and user controllers with routes for fetching chats, messages, and users

--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { getMessages } from "../controllers/messageController";
+import { protectRoute } from "../middleware/auth.ts";
 
 const router = Router()
 
+router.get("/chat/:chatId",protectRoute, getMessages)
 
 export default router;
