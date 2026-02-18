719de4a0ca634163290c264ecae8461ba3db42e5
Rishiraj Singh <rishirajsinghrathore029@gmail.com>
2026-02-18 15:58:24 +0530

    fix: add check for VITE_API_URL environment variable in axios configuration

--- web/src/lib/axios.js
+++ web/src/lib/axios.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+if(!apiUrl) {
+  console.log("VITE_API_URL environment variable is not set");
+  
+}
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + '/api',
   withCredentials: true,
